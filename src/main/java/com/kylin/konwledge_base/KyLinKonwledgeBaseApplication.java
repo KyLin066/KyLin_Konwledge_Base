76dc48c4850a4603503041e53870ebbb0312c5ea
KyLin <kylin066@gmail.com>
2022-05-03 10:11:30 +0800

    Initial commit

---
+++ src/main/java/com/kylin/konwledge_base/KyLinKonwledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.kylin.konwledge_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyLinKonwledgeBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyLinKonwledgeBaseApplication.class, args);
+    }
+
+}
