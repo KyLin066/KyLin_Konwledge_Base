716f968d7c153bd091e0842a6f06c1b03341cc64
KyLin <kylin066@gmail.com>
2022-05-09 22:35:22 +0800

    2.6 增加通用的返回类CommonResp，以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/com/kylin/knowledge_base/controller/EbookController.java
+++ src/main/java/com/kylin/knowledge_base/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.kylin.knowledge_base.controller;
 
 import com.kylin.knowledge_base.domain.Ebook;
+import com.kylin.knowledge_base.resp.CommonResp;
 import com.kylin.knowledge_base.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
