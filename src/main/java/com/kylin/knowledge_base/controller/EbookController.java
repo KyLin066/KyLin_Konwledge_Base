2b9bb9f0d320e69d5d42aefa34ed088aa688d3cc
KyLin <kylin066@gmail.com>
2022-05-11 13:51:02 +0800

    2.7 封装请求参数和返回参数

--- src/main/java/com/kylin/knowledge_base/controller/EbookController.java
+++ src/main/java/com/kylin/knowledge_base/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.kylin.knowledge_base.controller;
 
-import com.kylin.knowledge_base.domain.Ebook;
+import com.kylin.knowledge_base.req.EbookReq;
 import com.kylin.knowledge_base.resp.CommonResp;
+import com.kylin.knowledge_base.resp.EbookResp;
 import com.kylin.knowledge_base.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/kylin/knowledge_base/service/EbookService.java
+++ src/main/java/com/kylin/knowledge_base/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.kylin.knowledge_base.service;
 
 import com.kylin.knowledge_base.domain.Ebook;
+import com.kylin.knowledge_base.domain.EbookExample;
 import com.kylin.knowledge_base.mapper.EbookMapper;
+import com.kylin.knowledge_base.req.EbookReq;
+import com.kylin.knowledge_base.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +17,19 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
