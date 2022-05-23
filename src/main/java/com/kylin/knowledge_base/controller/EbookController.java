4d24b1157347411ff2753b629b5df4cb7ed4461f
KyLin <kylin066@gmail.com>
2022-05-23 16:50:42 +0800

    5.3 封装分页请求参数PageReq和返回参数PageResp

--- src/main/java/com/kylin/knowledge_base/controller/EbookController.java
+++ src/main/java/com/kylin/knowledge_base/controller/EbookController.java
@@ -3,13 +3,13 @@ package com.kylin.knowledge_base.controller;
 import com.kylin.knowledge_base.req.EbookReq;
 import com.kylin.knowledge_base.resp.CommonResp;
 import com.kylin.knowledge_base.resp.EbookResp;
+import com.kylin.knowledge_base.resp.PageResp;
 import com.kylin.knowledge_base.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ebook")
@@ -20,8 +20,8 @@ public class EbookController {
 
     @GetMapping("/list")
     public CommonResp list(EbookReq req) {
-        CommonResp<List<EbookResp>> resp = new CommonResp<>();
-        List<EbookResp> list = ebookService.list(req);
+        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
+        PageResp<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/kylin/knowledge_base/req/EbookReq.java
+++ src/main/java/com/kylin/knowledge_base/req/EbookReq.java
@@ -1,6 +1,6 @@
 package com.kylin.knowledge_base.req;
 
-public class EbookReq {
+public class EbookReq extends PageReq {
     private Long id;
 
     private String name;
