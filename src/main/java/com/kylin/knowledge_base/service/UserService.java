c17705530dc5ec42bd6cba61da0d0c20ad62c2f7
KyLin <kylin066@gmail.com>
2022-05-29 22:09:04 +0800

    9.4 完成用户表基本增删改查功能

---
+++ src/main/java/com/kylin/knowledge_base/service/UserService.java
@@ -0,0 +1,84 @@
+package com.kylin.knowledge_base.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.kylin.knowledge_base.domain.User;
+import com.kylin.knowledge_base.domain.UserExample;
+import com.kylin.knowledge_base.mapper.UserMapper;
+import com.kylin.knowledge_base.req.UserQueryReq;
+import com.kylin.knowledge_base.req.UserSaveReq;
+import com.kylin.knowledge_base.resp.UserQueryResp;
+import com.kylin.knowledge_base.resp.PageResp;
+import com.kylin.knowledge_base.util.CopyUtil;
+import com.kylin.knowledge_base.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<UserQueryResp> list(UserQueryReq req) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        if (!ObjectUtils.isEmpty(req.getLoginName())) {
+            criteria.andLoginNameEqualTo(req.getLoginName());
+        }
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<User> userList = userMapper.selectByExample(userExample);
+
+        PageInfo<User> pageInfo = new PageInfo<>(userList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        // List<UserResp> respList = new ArrayList<>();
+        // for (User user : userList) {
+        //     // UserResp userResp = new UserResp();
+        //     // BeanUtils.copyProperties(user, userResp);
+        //     // 对象复制
+        //     UserResp userResp = CopyUtil.copy(user, UserResp.class);
+        //
+        //     respList.add(userResp);
+        // }
+
+        // 列表复制
+        List<UserQueryResp> list = CopyUtil.copyList(userList, UserQueryResp.class);
+
+        PageResp<UserQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+
+        return pageResp;
+    }
+
+    /**
+     * 保存
+     */
+    public void save(UserSaveReq req) {
+        User user = CopyUtil.copy(req, User.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            user.setId(snowFlake.nextId());
+            userMapper.insert(user);
+        } else {
+            // 更新
+            userMapper.updateByPrimaryKey(user);
+        }
+    }
+
+    public void delete(Long id) {
+        userMapper.deleteByPrimaryKey(id);
+    }
+}
