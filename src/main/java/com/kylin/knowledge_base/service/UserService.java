8baa6eb4aa056f00e473a1ea8613d150cf5a1663
KyLin <kylin066@gmail.com>
2022-05-31 15:16:26 +0800

    9.5 增加校验用户名不能重复，增加自定义异常

--- src/main/java/com/kylin/knowledge_base/service/UserService.java
+++ src/main/java/com/kylin/knowledge_base/service/UserService.java
@@ -4,16 +4,19 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.kylin.knowledge_base.domain.User;
 import com.kylin.knowledge_base.domain.UserExample;
+import com.kylin.knowledge_base.exception.BusinessException;
+import com.kylin.knowledge_base.exception.BusinessExceptionCode;
 import com.kylin.knowledge_base.mapper.UserMapper;
 import com.kylin.knowledge_base.req.UserQueryReq;
 import com.kylin.knowledge_base.req.UserSaveReq;
-import com.kylin.knowledge_base.resp.UserQueryResp;
 import com.kylin.knowledge_base.resp.PageResp;
+import com.kylin.knowledge_base.resp.UserQueryResp;
 import com.kylin.knowledge_base.util.CopyUtil;
 import com.kylin.knowledge_base.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -69,9 +72,15 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(req.getId())) {
-            // 新增
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            User userDB = selectByLoginName(req.getLoginName());
+            if (ObjectUtils.isEmpty(userDB)) {
+                // 新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            } else {
+                // 用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
         } else {
             // 更新
             userMapper.updateByPrimaryKey(user);
@@ -80,5 +89,17 @@ public class UserService {
 
     public void delete(Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String LoginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(LoginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 }
