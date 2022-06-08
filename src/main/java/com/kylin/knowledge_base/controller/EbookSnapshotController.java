8561643afa6c920a3323fe004c6e260678d0f39c
KyLin <kylin066@gmail.com>
2022-06-08 20:02:30 +0800

    11.7 后端增加获取统计数值接口

---
+++ src/main/java/com/kylin/knowledge_base/controller/EbookSnapshotController.java
@@ -0,0 +1,29 @@
+package com.kylin.knowledge_base.controller;
+
+import com.kylin.knowledge_base.resp.CommonResp;
+import com.kylin.knowledge_base.resp.StatisticResp;
+import com.kylin.knowledge_base.service.EbookSnapshotService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook-snapshot")
+public class EbookSnapshotController {
+
+    @Resource
+    private EbookSnapshotService ebookSnapshotService;
+
+    @GetMapping("/get-statistic")
+    public CommonResp getStatistic() {
+        List<StatisticResp> statisticResp = ebookSnapshotService.getStatistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
+
+}
+
--- src/main/java/com/kylin/knowledge_base/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/kylin/knowledge_base/mapper/EbookSnapshotMapperCust.java
@@ -1,5 +1,11 @@
 package com.kylin.knowledge_base.mapper;
 
+import com.kylin.knowledge_base.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
